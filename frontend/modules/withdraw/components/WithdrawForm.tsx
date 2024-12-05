170df77c54cc51c15a8d3b2ca4e2fcedf0959566
chandreshpatidar <chandreshpatidar5@gmail.com>
2024-12-05 18:51:32 +0530

    Integrate API for withdraw

--- frontend/modules/withdraw/components/WithdrawForm.tsx
+++ frontend/modules/withdraw/components/WithdrawForm.tsx
@@ -1,29 +1,50 @@
 'use client';
 import React from 'react';
-import { Form, Formik } from 'formik';
+import { Form, Formik, FormikValues } from 'formik';
 import FormikInput from '@/components/formik/FormikInput';
 import { Button } from '@/components/ui/button';
 import { withdrawFormValidationSchema } from '../validations/withdrawFormValidationSchema';
+import { WithdrawMoneyInput } from '@/modules/account/types';
+import { useAccountStore } from '@/modules/account/store/accountStore';
+import { useToast } from '@/hooks/use-toast';
 
 interface WithdrawFormProps {
   onCancel: () => void;
+  onSuccess?: () => void;
 }
 
-export const WithdrawForm: React.FC<WithdrawFormProps> = ({ onCancel }) => {
+export const WithdrawForm: React.FC<WithdrawFormProps> = ({ onCancel, onSuccess }) => {
+  const { withdraw } = useAccountStore();
+  const { showSuccessToast, showErrorToast } = useToast();
+
+  const onSubmit = async (values: FormikValues) => {
+    const error = await withdraw({ amount: Number(values.amount) } as WithdrawMoneyInput);
+
+    if (error?.message) {
+      showErrorToast(error.message);
+    } else {
+      showSuccessToast('Withdrawal successful');
+      onSuccess?.();
+    }
+  };
+
   return (
     <Formik
       validationSchema={withdrawFormValidationSchema}
       initialValues={{
-        amount: 0,
+        amount: '',
         message: '',
       }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values) => onSubmit(values)}
     >
       <Form className='flex flex-col gap-4 w-full'>
         <div className='grid grid-cols-1 gap-4'>
           <FormikInput
             name='amount'
             label='Amount *'
+            type='number'
+            min={0}
+            placeholder='0'
           />
         </div>
 
