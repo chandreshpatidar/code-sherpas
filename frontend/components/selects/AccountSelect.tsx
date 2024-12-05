970049f764a02276a99eba3616916dcff59558fc
chandreshpatidar <chandreshpatidar5@gmail.com>
2024-12-05 16:13:29 +0530

    Show dynamic options account select

--- frontend/components/selects/AccountSelect.tsx
+++ frontend/components/selects/AccountSelect.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
+import { useAppContext } from '@/store/app';
+import { useAccountStore } from '@/modules/account/store/accountStore';
 
 export const AccountSelect = () => {
+  const { state } = useAppContext();
+  const { fetchUserAccounts, setActiveAccount, activeAccount, accounts } = useAccountStore();
+
+  const onValueChange = (id: string) => {
+    const account = accounts.find((account) => account.id === id)!;
+
+    setActiveAccount(account);
+  };
+
+  useEffect(() => {
+    if (state.user?.id) {
+      fetchUserAccounts(state.user.id);
+    }
+  }, [state.user?.id]);
+
   return (
-    <Select>
+    <Select
+      value={activeAccount?.id}
+      onValueChange={onValueChange}
+    >
       <SelectTrigger className='w-full'>
         <SelectValue placeholder='Select an account' />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value='1'>Account 1</SelectItem>
-        <SelectItem value='2'>Account 2</SelectItem>
-        <SelectItem value='3'>Account 3</SelectItem>
-        <SelectItem value='4'>Account 4</SelectItem>
+        {accounts.map((account) => (
+          <SelectItem
+            key={account.id}
+            value={account.id}
+          >
+            <div className='w-full flex items-center'>
+              <label className='capitalize'>{account.id}</label>
+            </div>
+          </SelectItem>
+        ))}
       </SelectContent>
     </Select>
   );
