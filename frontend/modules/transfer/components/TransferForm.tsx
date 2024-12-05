089d15e33d81cdb8d6e517c297f09d53e57141a0
chandreshpatidar <chandreshpatidar5@gmail.com>
2024-12-05 18:39:55 +0530

    Integrate API for transfer

--- frontend/modules/transfer/components/TransferForm.tsx
+++ frontend/modules/transfer/components/TransferForm.tsx
@@ -1,34 +1,63 @@
 'use client';
-import React from 'react';
-import { Form, Formik } from 'formik';
+import React, { useMemo } from 'react';
+import { Form, Formik, FormikValues } from 'formik';
 import FormikInput from '@/components/formik/FormikInput';
 import FormikSelect from '@/components/formik/FormikSelect';
 import { Button } from '@/components/ui/button';
 import { transferFormValidationSchema } from '../validations/transferFormValidationSchema';
+import { useAccountStore } from '@/modules/account/store/accountStore';
+import { useToast } from '@/hooks/use-toast';
+import { TransferMoneyInput } from '@/modules/account/types';
 
 interface TransferFormProps {
   onCancel: () => void;
+  onSuccess?: () => void;
 }
 
-export const TransferForm: React.FC<TransferFormProps> = ({ onCancel }) => {
+export const TransferForm: React.FC<TransferFormProps> = ({ onCancel, onSuccess }) => {
+  const { transfer, accounts, activeAccount } = useAccountStore();
+  const { showSuccessToast, showErrorToast } = useToast();
+
+  const recipients = useMemo(
+    () =>
+      accounts.reduce((acc, account) => {
+        if (account.id !== activeAccount?.id) {
+          acc.push({ label: account.id, value: account.id });
+        }
+        return acc;
+      }, [] as { label: string; value: string }[]),
+    [accounts, activeAccount]
+  );
+
+  const onSubmit = async (values: FormikValues) => {
+    const error = await transfer({
+      amount: Number(values.amount),
+      toAccountId: values.recipient,
+    } as TransferMoneyInput);
+
+    if (error?.message) {
+      showErrorToast(error.message);
+    } else {
+      showSuccessToast('Money transfer successful');
+      onSuccess?.();
+    }
+  };
+
   return (
     <Formik
       validationSchema={transferFormValidationSchema}
       initialValues={{
-        amount: 0,
+        amount: '',
         recipient: '',
         message: '',
       }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values) => onSubmit(values)}
     >
       <Form className='flex flex-col gap-4 w-full'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
           <FormikSelect
             name='recipient'
-            options={[
-              { label: 'Option 1', value: 'option1' },
-              { label: 'Option 2', value: 'option2' },
-            ]}
+            options={recipients}
             label='Select Recipient Account*'
           />
 
@@ -36,6 +65,8 @@ export const TransferForm: React.FC<TransferFormProps> = ({ onCancel }) => {
             name='amount'
             label='Amount *'
             type='number'
+            min={0}
+            placeholder='0'
           />
         </div>
 
