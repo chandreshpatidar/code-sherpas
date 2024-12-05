9940531a8aa8168de26ccd15f3eac76159541941
chandreshpatidar <chandreshpatidar5@gmail.com>
2024-12-05 16:47:49 +0530

    Update styles

--- frontend/app/(protected)/transactions/page.tsx
+++ frontend/app/(protected)/transactions/page.tsx
@@ -1,5 +1,5 @@
 import Layout from '@/components/layout/Layout';
-import { BankingQuickActions } from '@/modules/banking/components';
+import { BankingQuickActions } from '@/modules/account/components';
 import { PaginatedTransactions } from '@/modules/transactions/components/PaginatedTransactions';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
@@ -8,10 +8,10 @@ export default function TransactionPage() {
   return (
     <Layout>
       <BankingQuickActions />
-      <div className='container mx-auto'>
+      <div className='container mx-auto max-w-xl'>
         <Link
           href={'/'}
-          className='flex items-center gap-3'
+          className='flex items-center gap-3 mb-5 md:mb-8'
         >
           <ArrowLeft size={24} /> Back to dashboard
         </Link>
--- frontend/modules/dashboard/components/AccountSnapshot.tsx
+++ frontend/modules/dashboard/components/AccountSnapshot.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { AccountSelect } from '@/components/selects/AccountSelect';
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { useAccountStore } from '@/modules/account/store/accountStore';
 import { RecentTransactions } from '@/modules/transactions/components';
 import { useAppContext } from '@/store/app';
 import React from 'react';
@@ -9,19 +11,25 @@ export const AccountSnapshot = () => {
   const {
     state: { user },
   } = useAppContext();
+  const { activeAccount } = useAccountStore();
 
   return (
     <>
       <h2 className='text-2xl'>Welcome {user?.name}</h2>
 
-      <div className='mt-8 w-full flex flex-col lg:flex-row flex-wrap gap-10'>
-        <div className='flex-1 max-w-lg flex flex-col gap-4'>
+      <div className='mt-8 w-full flex flex-col lg:flex-row flex-wrap gap-6 lg:gap-10'>
+        <div className='flex-1 lg:max-w-lg flex flex-col gap-6'>
           <AccountSelect />
 
-          <Card className='h-fit'>
+          <Card
+            className={cn('h-fit transition-all', {
+              'opacity-40': !activeAccount?.id,
+              'opacity-100': activeAccount?.id,
+            })}
+          >
             <CardHeader>
               <CardDescription>Current Balance</CardDescription>
-              <CardTitle>$123</CardTitle>
+              <CardTitle>{activeAccount?.id ? `$${activeAccount.balance}` : '$0.00'}</CardTitle>
             </CardHeader>
           </Card>
         </div>
